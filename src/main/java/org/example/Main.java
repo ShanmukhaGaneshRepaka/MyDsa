35c440ca1b5d2121781590e1b0f5dfc1f1ed5158
Ganesh <repakashanmukhaganesh@gmail.com>
2025-03-10 11:38:17 +0530

    patterns committed sucessfully

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,28 @@
+package org.example;
+
+import java.util.Arrays;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+  int arr[] ={11,-5,5,6};
+        generateSubarrays(arr);
+    }
+
+    static void generateSubarrays(int arr[]){
+
+int  maxSum=0;
+        for(int i=0;i<arr.length;i++){
+            int currentSum = 0;
+            for(int j=i;j<arr.length;j++){
+               currentSum+=arr[j];
+
+            }
+
+          maxSum=Math.max(maxSum,currentSum);
+
+        }
+        System.out.println(maxSum);
+    }
+}
